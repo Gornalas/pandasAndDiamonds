23111ce812be5a944fc0d6ee615e8195a7f46163
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2022-07-18 12:55:00 +0300

    Update readCsv.py

--- readCsv.py
+++ readCsv.py
@@ -1,8 +1,8 @@
 #Read first 5 rows from csv
 
 import pandas as pd
-pd.set_option('display.max_rows', 50)
-pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 250)
+pd.set_option('display.max_columns', 10)
 diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
 print("First 5 rows:")
-print(diamonds.head())
+print(diamonds.head(5))
