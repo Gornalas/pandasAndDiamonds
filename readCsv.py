6f24b000789755e501d11218d7ce9ffbc13df0c0
Alexey Kryga <48469412+Gornalas@users.noreply.github.com>
2022-07-18 12:57:53 +0300

    Read first 5 rows from csv

--- readCsv.py
+++ readCsv.py
@@ -1,8 +1,9 @@
-#Read first 5 rows from csv
-
 import pandas as pd
+
+#options for EDI
 pd.set_option('display.max_rows', 250)
 pd.set_option('display.max_columns', 10)
 diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
 print("First 5 rows:")
+#if you need only 5 first
 print(diamonds.head(5))
